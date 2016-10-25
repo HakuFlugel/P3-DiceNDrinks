72c80652e120bef6ef56a99bda614ee9e5aacdb1
joakim <joakim347@gmail.com>
2016-10-25 14:33:09 +0200

    Fix adminproject output Created form and added Tab classes

---
+++ AdministratorPanel/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace AdministratorPanel {
+    class Program {
+
+        private Form form;
+
+
+        public Program() {
+            form = new Form();
+            form.Width = 1400;
+            form.Height = 1000;
+
+            
+
+            TabControl cp = new TabControl();
+            cp.Dock = DockStyle.Fill;
+
+           TabPage[] tabs = {
+                    new TabPage("Calendar"),
+                    new TabPage("Products"),
+                    new TabPage("Games"),
+                    new TabPage("Events")
+            };
+
+
+            cp.Controls.AddRange(tabs);
+            form.Controls.Add(cp);
+
+            form.Activate();
+            form.Show();
+
+
+        }
+
+        private void Start() {
+            Application.Run();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        static void Main(string[] args) {
+
+            Program p = new Program();
+            p.Start();
+        }
+
+
+    }
+}
