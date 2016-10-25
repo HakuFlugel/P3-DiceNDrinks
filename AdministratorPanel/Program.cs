b0a5811e37d90140f3881e82e47cad4d8c462974
Asger <Asger@DESKTOP-6QCNFFC>
2016-10-25 19:14:47 +0200

    Added some stuff for testing purposes. And added a "feature" so that it is impossible to resize the window.

--- AdministratorPanel/Program.cs
+++ AdministratorPanel/Program.cs
@@ -9,24 +9,33 @@ using System.Drawing;
 
 namespace AdministratorPanel {
     class Program {
-
+        private int width = 1200;
+        private int height = 750;
         private Form form;
 
 
         public Program() {
+            // For testing purpose only
+            List<Games> games = new List<Games>();
+            games.Add(new Games("AHZ2xB", "Secret Hitler", "Horror", "A game about gaming", 2014, 5, 10, 30, 60, "TosetPictureInFuture"));
+            games.Add(new Games("J8AkkS", "Dominion", "Horror", "A game about gaming", 2014, 5, 10, 30, 60, "TosetPictureInFuture"));
+            games.Add(new Games("5ExgGS", "Small Worlds", "Horror", "A game about gaming", 2014, 5, 10, 30, 60, "TosetPictureInFuture"));
+            games.Add(new Games("TYE3sj", "Enter The Gundion", "Horror", "A game about gaming", 2014, 5, 10, 30, 60, "TosetPictureInFuture"));
+
+
             form = new Form();
-            form.Width = 1400;
-            form.Height = 1000;
+            form.Width = width;
+            form.Height = height;
 
             
 
             TabControl cp = new TabControl();
             cp.Dock = DockStyle.Fill;
 
-           TabPage[] tabs = {
+            TabPage[] tabs = {
                     new CalendarTab(),
                     new ProductsTab(),
-                    new GamesTab(),
+                    new GamesTab(form, games),
                     new EventsTab()
             };
 
@@ -34,10 +43,19 @@ namespace AdministratorPanel {
             cp.Controls.AddRange(tabs);
             form.Controls.Add(cp);
 
+
             form.Activate();
             form.Show();
+            form.Resize += Form_Resize;
 
 
+
+        }
+
+        private void Form_Resize(object sender, EventArgs e) {
+
+            form.Width = width;
+            form.Height = height;
         }
 
         private void Start() {
