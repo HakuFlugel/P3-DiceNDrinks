b9b5cc6ad445b05cc34261a5acabae6d51956b04
Xiazee <Xiazee@users.noreply.github.com>
2016-10-25 12:38:36 +0200

    Added fields to shared classes

--- Shared/Game.cs
+++ Shared/Game.cs
@@ -1,7 +1,21 @@
-﻿namespace Shared
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Shared
 {
+    [Serializable]
     public class Game
     {
+        public string id;
 
+        public string name;
+        public string description;
+        public int publishedYear;
+
+        public int minPlayers, maxPlayers;
+        public int minPlayTime, maxPlayTime;
+
+        public string thumbnail, image;
     }
 }
