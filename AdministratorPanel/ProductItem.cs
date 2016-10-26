fe08399c615b3ba8e91c8335004703a3dc5bee69
joakim <joakim347@gmail.com>
2016-10-26 19:49:29 +0200

    More UI products

--- AdministratorPanel/ProductItem.cs
+++ AdministratorPanel/ProductItem.cs
@@ -8,54 +8,88 @@ using Shared;
 using System.Drawing;
 
 namespace AdministratorPanel {
-    class ProductItem : Button{
+    class ProductItem : TableLayoutPanel {
 
         private int sizeX = 160;
         private int sizeY = 160;
-        TableLayoutPanel tb = new TableLayoutPanel();
-        
 
-        private string name;
         private Image image = Image.FromFile("C:/Users/Bruger/Desktop/Red.png");
-        private List<PriceElement> priceElements;
 
         public ProductItem(Product p) {
             Update();
             //table();
-            this.name = p.name;
-            this.Text = Information();
-            this.priceElements = p.PriceElements;
-            this.Image = image;
-            this.TextImageRelation = TextImageRelation.ImageAboveText;
-            
+            //this.name = p.name;
+            ////this.Text = Information();
+            //this.priceElements = p.PriceElements;
+            ////this.Image = image;
+
+            //BorderStyle = BorderStyle.FixedSingle;
+            BackColor = Color.LightGray;
+            GrowStyle = TableLayoutPanelGrowStyle.FixedSize; // note skal måske ændres
+            RowCount = 3;
+            ColumnCount = 1;
+
+            //1
+            PictureBox pb = new PictureBox();
+            pb.Image = image;
+            Controls.Add(pb);
+
+            //2
+            Label lb = new Label();
+            lb.Text = p.name;
+            Controls.Add(lb);
+
+            //3
+            TableLayoutPanel tb = new TableLayoutPanel();
+            tb.AutoSize = true;
+            tb.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+                //RowCount = 0;
+            ColumnCount = 2;
+            Controls.Add(Information(p,tb));
+
+            //done
+
+            this.MakeSuperClickable((sender, ev) => {Console.WriteLine("Loli-pop"); });
         }
 
-        private string Information() {
+        private TableLayoutPanel Information(Product p, TableLayoutPanel tb) {
             StringBuilder sb = new StringBuilder();
-            sb.AppendLine(name);
-            //sb.
-            foreach (var item in priceElements) {
-                sb.AppendLine(item.name + "Price: " + item.price);
-            }
-            
-            return sb.ToString();
+            Label temp = new Label();
+            List<Label> lList = new List<Label>();
 
+            sb.AppendLine(p.name);
             
+            foreach (var item in p.PriceElements) {
+                sb.Append(item.name);
+                temp.Text = sb.ToString();
+                lList.Add(temp);
+                sb.Clear();
+
+                sb.Append("Price: ");
+                sb.Append(item.name);
+                temp.Text = sb.ToString();
+                lList.Add(temp);
+                sb.Clear();
+            }
+
+            //foreach (var item in lList) {
+            //    Console.WriteLine(item.Text);
+            //}
+
+            tb.Controls.AddRange(lList.ToArray());
+            return tb;
         }
 
         private void table() {
-            tb.GrowStyle = TableLayoutPanelGrowStyle.FixedSize;
-            tb.Dock = DockStyle.Fill;
-            tb.RowCount = 2;
-            tb.ColumnCount = 2;
-            tb.Name = "Information";
-            tb.BackColor = Color.Transparent;
-            tb.Controls.Add(new Button { Height = 20, Width = 20, Text = "1" });
-            tb.Controls.Add(new Button { Height = 20, Width = 20, Text = "2" });
-            tb.Controls.Add(new Button { Height = 20, Width = 20, Text = "3" });
-            tb.Controls.Add(new Button { Height = 20, Width = 20, Text = "4" });
-            Controls.Add(tb);
 
+            Name = "Information";
+            BackColor = Color.Transparent;
+            Panel p = new Panel();
+
+            Controls.Add(new Button { Height = 20, Width = 20, Text = "1" });
+            Controls.Add(new Button { Height = 20, Width = 20, Text = "2" });
+            Controls.Add(new Button { Height = 20, Width = 20, Text = "3" });
+            Controls.Add(new Button { Height = 20, Width = 20, Text = "4" });
         }
 
         private void Update(){
@@ -63,10 +97,41 @@ namespace AdministratorPanel {
             this.Width = sizeX;
             
         }
-        protected override void OnClick(EventArgs e) {
-            base.OnClick(e);
-            Console.WriteLine(this);
-        }
 
+
+
+
+
+
+
+
+        //protected override void OnClick(EventArgs e) {
+        //    base.OnClick(e);
+        //    Console.WriteLine("clicked " + this);
+        //}
+
+        //private void ControlAdded(Control control) {
+
+        //}
+
+        //protected override void OnControlAdded(ControlEventArgs e) {
+        //    EventHandler click = (sender, ev) => { OnClick(ev); };
+        //    ControlEventHandler controladded = (sender, ev) => {
+        //        //OnControlAdded(ev);
+
+        //        foreach (Control control in e.Control.Controls) {
+        //            ControlEventArgs cev = new ControlEventArgs(control);
+        //            OnControlAdded(cev);
+        //            //controladded(this, cev);
+        //        }
+        //    };
+
+        //    e.Control.Click -= click;
+        //    e.Control.Click += click;
+        //    e.Control.ControlAdded -= controladded;
+        //    e.Control.ControlAdded += controladded;
+
+        //    base.OnControlAdded(e);
+        //}
     }
 }
