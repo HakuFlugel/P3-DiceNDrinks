b250f616edefdcc939676e9fa0c219f7ea8d480a
Xiazee <Xiazee@users.noreply.github.com>
2016-10-26 16:07:45 +0200

    Changed width of NiceTextBox
    Created PendingReservationList

--- AdministratorPanel/CalendarTab.cs
+++ AdministratorPanel/CalendarTab.cs
@@ -15,8 +15,7 @@ namespace AdministratorPanel {
             outerTable.ColumnCount = 2;
             Controls.Add(outerTable);
 
-
-            // Left Side
+            //// Left Side
             TableLayoutPanel leftTable = new TableLayoutPanel();
             leftTable.Dock = DockStyle.Left;
             leftTable.AutoSize = true;
@@ -24,45 +23,15 @@ namespace AdministratorPanel {
             //leftTable.BorderStyle = BorderStyle.Fixed3D;
             outerTable.Controls.Add(leftTable);
 
-
-
             calendar = new Calendar();
             //calendar.Dock = DockStyle.Top;
             //calendar.Anchor = AnchorStyles.Top;
             leftTable.Controls.Add(calendar);
 
-            FlowLayoutPanel b = new FlowLayoutPanel();
-            b.Text = "Something";
-            b.Dock = DockStyle.Fill;
-            b.BorderStyle = BorderStyle.Fixed3D;
-            b.FlowDirection = FlowDirection.TopDown;
-            b.WrapContents = false;
-            b.AutoScroll = true;
+            PendingReservationList b = new PendingReservationList();
             leftTable.Controls.Add(b);
-            //b.scroll
 
-            for (int i = 0; i < 100; i++)
-            {
-                Button button = new Button();
-                button.Text = "66/6 6666\nLars | 42\nJens | 66";
-                //button.Height = 666;
-                button.Height = (int)(button.Height * 3 / 1.5);
-                //button.Anchor = AnchorStyles.Left & AnchorStyles.Right & AnchorStyles.Top;
-                //button.Dock = DockStyle.Fill;
-                //button.AutoSizeMode = AutoSizeMode.GrowOnly;
-
-                b.Controls.Add(button);
-                button.Width = button.Parent.ClientSize.Width - 20;
-                TextBox t = new TextBox();
-
-
-            }
-
-            //b.Anchor = AnchorStyles.Top & AnchorStyles.Left & AnchorStyles.Right & AnchorStyles.Bottom;
-
-
-
-            // Right side
+            //// Right side
             TableLayoutPanel rightTable = new TableLayoutPanel();
             rightTable.GrowStyle = TableLayoutPanelGrowStyle.AddColumns;
             rightTable.Dock = DockStyle.Fill;
