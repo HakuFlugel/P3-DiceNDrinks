57c5eaa44781d90da85425f79ddb9573e5f7d871
GeneralDenmark <foo-fighters@live.dk>
2016-10-26 17:12:48 +0200

    Added buttons.

--- AdministratorPanel/FansyPopupBox.cs
+++ AdministratorPanel/FansyPopupBox.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace AdministratorPanel {
     abstract class FansyPopupBox {
@@ -24,10 +25,38 @@ namespace AdministratorPanel {
 
 
         }
+        
         public void openMessageBox() {
+
+            defaultButtons();
             messageBox.Text = sTitle;
             messageBox.Controls.AddRange(control.ToArray());
+           
+        }
 
+        private void defaultButtons() {
+            Point startLoacttion = new Point(12, 393);
+
+            for(int i = 0; i < 2; i++) {
+                Button[] buttons = { new Button(), new Button(), new Button() };
+                buttons[0].Click += cancle; buttons[1].Click += Delete;
+                foreach(var item in buttons) {
+                    item.Size = new Size(134, 48);
+                    item.Location = new Point(startLoacttion.X, startLoacttion.Y);
+                    startLoacttion.X += 318;
+                    
+                }
+                
+
+            }
+        }
+
+        private void Delete(object sender, EventArgs e) {
+            throw new NotImplementedException();
+        }
+
+        private void cancle(object sender, EventArgs e) {
+            messageBox.Close();
         }
     }
 }
