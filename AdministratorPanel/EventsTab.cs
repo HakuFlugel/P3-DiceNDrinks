86fc6f6504928cd5c3c744abb3da7521e00b06cf
hidefades <hiddenfades@gmail.com>
2016-10-26 15:34:40 +0200

    EventTab

--- AdministratorPanel/EventsTab.cs
+++ AdministratorPanel/EventsTab.cs
@@ -1,10 +1,31 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.Windows.Forms;
 
 namespace AdministratorPanel {
     public class EventsTab : TabPage {
-        public EventsTab() {
+        Form form;
+        public EventsTab(Form form) {
+            this.form = form;
             Text = "Events";
+            
+            Button newEvent = new Button {
+                Height = 20,
+                Width = 100,
+                Text = "New Event",
+                Location = new Point((form.Width / 10) * 8 + (Width / 4), 10)};
 
+            TableLayoutPanel tlp = new TableLayoutPanel();
+            tlp.Dock = DockStyle.Left;
+            tlp.BackColor = Color.Transparent;
+            tlp.Width = (form.Width / 10) * 8 + (newEvent.Width / 4);
+            
+            
+            Controls.Add(newEvent);
+            tlp.Controls.Add(new Button { Height = 20, Width = 100, Text = "Test :D" });
+            Controls.Add(tlp);
+            
         }
+
+
     }
 }
