56116512860a9e6b4aa76867e08bef45b041b7ab
GeneralDenmark <foo-fighters@live.dk>
2016-10-26 10:32:31 +0200

    Nice textbox with watermarks :3

---
+++ AdministratorPanel/NiceTextBox.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdministratorPanel {
+    class NiceTextBox : TextBox {
+        public string waterMark = "";
+        public NiceTextBox(string waterMark) {
+            this.waterMark = waterMark;
+            Text = waterMark;
+        }
+        protected override void OnGotFocus(EventArgs e) {
+            if(waterMark != "") {
+                Text = "";
+            }
+            base.OnGotFocus(e);
+        }
+        protected override void OnLostFocus(EventArgs e) {
+            if(Text == "") {
+                Text = waterMark;
+            }
+            base.OnLostFocus(e);
+        }
+    }
+}
