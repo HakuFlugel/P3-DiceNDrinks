66c715ba5cb8929273032a050763b843a97e3fc4
hidefades <hiddenfades@gmail.com>
2016-10-26 16:08:42 +0200

    Pushing update Eventtab

--- AdministratorPanel/EventsTab.cs
+++ AdministratorPanel/EventsTab.cs
@@ -8,21 +8,29 @@ namespace AdministratorPanel {
             this.form = form;
             Text = "Events";
             
-            Button newEvent = new Button {
+            /*Button newEvent = new Button {
                 Height = 20,
                 Width = 100,
                 Text = "New Event",
-                Location = new Point((form.Width / 10) * 8 + (Width / 4), 10)};
+                Location = new Point(form.Width - (Width / 4) * 3, 10)};*/
 
-            TableLayoutPanel tlp = new TableLayoutPanel();
-            tlp.Dock = DockStyle.Left;
-            tlp.BackColor = Color.Transparent;
-            tlp.Width = (form.Width / 10) * 8 + (newEvent.Width / 4);
-            
-            
-            Controls.Add(newEvent);
-            tlp.Controls.Add(new Button { Height = 20, Width = 100, Text = "Test :D" });
-            Controls.Add(tlp);
+            TableLayoutPanel headtlp = new TableLayoutPanel();
+            headtlp.Dock = DockStyle.Fill;
+            headtlp.BackColor = Color.Transparent;
+            headtlp.RowCount = 2;
+            headtlp.ColumnCount = 1;
+            FlowLayoutPanel topflp = new FlowLayoutPanel();
+            FlowLayoutPanel lowerflp = new FlowLayoutPanel();
+
+            topflp.Controls.Add(new Button { Height = 20, Width = 100, Text = "Test :D" });
+
+
+            headtlp.Controls.Add(topflp);
+            headtlp.Controls.Add(lowerflp);
+
+            //Controls.Add(newEvent);
+            //headtlp.Controls.Add(new Button { Height = 20, Width = 100, Text = "Test :D" });
+            Controls.Add(headtlp);
             
         }
 
