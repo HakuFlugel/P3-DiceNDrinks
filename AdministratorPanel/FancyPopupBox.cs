eca7ae8d785bfcaced84d896f2e5d3db38fb04d1
GeneralDenmark <foo-fighters@live.dk>
2016-10-28 13:39:29 +0200

    Kommenterede alt ud skov ikke ville have. Startede op en mere itterativ tilgang?

--- AdministratorPanel/FancyPopupBox.cs
+++ AdministratorPanel/FancyPopupBox.cs
@@ -1,45 +1,10 @@
-﻿/*using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-
-namespace AdministratorPanel {
-    public partial class ProductPopupbox : Form {
-        
-        public string sName = "";
-        public string sDescription = "";
-        public string sID = "";
-        public bool hasBeenEdi = false;
-        public string sImage = "";
-        public int pPrice = 0;
-
-        public ProductPopupbox() {
-            this.FormBorderStyle = FormBorderStyle.FixedSingle;
-            
-            InitializeComponent();
-        }
-        public void openMessageBox() {
-
-        }
-
-        
-    }
-}
-*/
-
-/*using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
+using System;
 
 namespace AdministratorPanel {
     abstract class FansyPopupBox : TableLayoutPanel {
@@ -52,33 +17,46 @@ namespace AdministratorPanel {
         public List<Control> control = new List<Control>();
 
         public FansyPopupBox() {
-            
+            TableLayoutPanel tb = new TableLayoutPanel();
+
             messageBox.FormBorderStyle = FormBorderStyle.FixedSingle;
             messageBox.Width = 800;
             messageBox.Height = 500;
-            control.Add(new Control());
+            //1?
+            GrowStyle = TableLayoutPanelGrowStyle.FixedSize; // note skal måske ændres
+            RowCount = 3;
+            ColumnCount = 1;
+            //2?
 
+
+            //3?
 
         }
         
+
         public void openMessageBox() {
 
             defaultButtons();
             messageBox.Controls.AddRange(control.ToArray());
-           
+
         }
 
         private void defaultButtons() {
             Point startLoacttion = new Point(12, 393);
 
-            for(int i = 0; i < 2; i++) {
+            for (int i = 0; i < 2; i++) {
                 Button[] buttons = { new Button(), new Button(), new Button() };
                 buttons[0].Click += cancle; buttons[1].Click += Delete; buttons[2].Click += Save;
-                foreach(var item in buttons) {
+
+                buttons[0].Location = new Point(496, 393);
+                buttons[1].Location = new Point(636, 393);
+                buttons[2].Location = new Point(12, 393);
+
+                foreach (var item in buttons) {
                     item.Size = new Size(134, 48);
                     item.Location = new Point(startLoacttion.X, startLoacttion.Y);
-                    startLoacttion.X += 318;
                     
+
                 }
 
             }
@@ -89,7 +67,7 @@ namespace AdministratorPanel {
         abstract public void Delete(object sender, EventArgs e);
 
         private void cancle(object sender, EventArgs e) {
-            if(hasBeenEdi) {
+            if (hasBeenEdi) {
                 if (DialogResult.Yes ==
                     MessageBox.Show("Are you sure? Everything unsaved will be lost.",
                     "About to close",
@@ -106,7 +84,7 @@ namespace AdministratorPanel {
         ProductPopupbox() {
             Text = "Product details";
             GrowStyle = TableLayoutPanelGrowStyle.FixedSize;
-            
+
             RowCount = 3;
             ColumnCount = 2;
 
@@ -143,20 +121,3 @@ namespace AdministratorPanel {
 
 
 }
-
-
-
-
-
-
-/*
-  public bool isGames = false;
-        //shared
-        
-
-        
-
-        
-
-
-     */
