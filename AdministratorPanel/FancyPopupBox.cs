63b7988fbbdacd55e796cc80a00f34da5156c28e
GeneralDenmark <foo-fighters@live.dk>
2016-10-28 14:58:22 +0200

    Stuff

--- AdministratorPanel/FancyPopupBox.cs
+++ AdministratorPanel/FancyPopupBox.cs
@@ -5,43 +5,69 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
 using System;
-
+using Shared;
 namespace AdministratorPanel {
-    abstract class FansyPopupBox : TableLayoutPanel {
-        private Form messageBox = new Form();
-        public string sName = "";
-        public string sDescription = "";
-        public string sID = "";
-        public bool hasBeenEdi = false;
-        public string sImage = "";
-        public List<Control> control = new List<Control>();
+    /*abstract*/
+    abstract class FansyPopupBox : Form {
+        public bool isChanged = false;
 
         public FansyPopupBox() {
-            TableLayoutPanel tb = new TableLayoutPanel();
 
-            messageBox.FormBorderStyle = FormBorderStyle.FixedSingle;
-            messageBox.Width = 800;
-            messageBox.Height = 500;
-            //1?
-            GrowStyle = TableLayoutPanelGrowStyle.FixedSize; // note skal måske ændres
-            RowCount = 3;
-            ColumnCount = 1;
+            MinimumSize = new Size(800, 400);
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+
+            TableLayoutPanel tb = new TableLayoutPanel();
+            tb.BackColor = Color.AliceBlue;
+            Controls.Add(tb);
+            tb.Dock = DockStyle.Fill;
+
+            tb.GrowStyle = TableLayoutPanelGrowStyle.FixedSize;
+            tb.RowCount = 2;
+            tb.ColumnCount = 1;
+
+            tb.Controls.Add(CreateControls());
+
+            /*TableLayoutPanel splitButtons = new TableLayoutPanel();
+            splitButtons.RowCount = 1;
+            splitButtons.ColumnCount = 2;
+            splitButtons.GrowStyle = TableLayoutPanelGrowStyle.FixedSize;*/
+            //tb.Controls.Add(splitButtons);
+
+            Panel p = new Panel();
+            p.Height = 42;
+            p.Dock = DockStyle.Bottom;
+            p.BackColor = Color.LightBlue;
+            tb.Controls.Add(p);
+
+            Button delete = new Button();
+            delete.Text = "Delete";
+            delete.Dock = DockStyle.Left;
+            p.Controls.Add(delete);
+            
+
+            Button cancel = new Button();
+            cancel.Text = "Cancel";
+            cancel.Dock = DockStyle.Right;
+            p.Controls.Add(cancel);
+
+            Button save = new Button();
+            save.Text = "Save";
+            save.Dock = DockStyle.Right;
+            p.Controls.Add(save);
+
+
+
             //2?
 
 
             //3?
 
         }
-        
 
-        public void openMessageBox() {
+        protected abstract Control CreateControls();
 
-            defaultButtons();
-            messageBox.Controls.AddRange(control.ToArray());
-
-        }
-
-        private void defaultButtons() {
+        /*private void defaultButtons() {
             Point startLoacttion = new Point(12, 393);
 
             for (int i = 0; i < 2; i++) {
@@ -60,9 +86,9 @@ namespace AdministratorPanel {
                 }
 
             }
-        }
+        }*/
 
-        abstract public void Save(object sender, EventArgs e);
+        /*abstract public void Save(object sender, EventArgs e);
 
         abstract public void Delete(object sender, EventArgs e);
 
@@ -76,48 +102,60 @@ namespace AdministratorPanel {
                     messageBox.Close();
                 }
             }
-        }
+        }*/
     }
+
     class ProductPopupbox : FansyPopupBox {
         //for products
         public int pRice;
-        ProductPopupbox() {
-            Text = "Product details";
-            GrowStyle = TableLayoutPanelGrowStyle.FixedSize;
-
-            RowCount = 3;
-            ColumnCount = 2;
-
-        }
-
-        public override void Delete(object sender, EventArgs e) {
+        Product product;
+        public ProductPopupbox(Product product) {
+            
+            this.product = product;
+            
 
         }
 
-        public override void Save(object sender, EventArgs e) {
+        protected override Control CreateControls() {
+            TableLayoutPanel tbtb = new TableLayoutPanel();
+            tbtb.GrowStyle = TableLayoutPanelGrowStyle.FixedSize;
+            tbtb.RowCount = 1;
+            tbtb.ColumnCount = 2;
 
-        }
-    }
+            Panel leftPanel = new Panel();
 
-    class GamesPopupbox : FansyPopupBox {
-        //for games
-        public int gYear;
-        public string gGenre = "";
-        public int gMinTime;
-        public int gMaxTime;
-        public int gMinPlayers;
-        public int gMaxPlayers;
+            NiceTextBox name = new NiceTextBox();
+            name.clearable = true;
+            name.waterMark = "Insert name....";
+            name.TextChanged += (sender, ev) => {
+                isChanged = true;
+            };
+            ComboBox stuff = new ComboBox();
 
-        GamesPopupbox() {
-            Text = "Game details";
-        }
-
-        public override void Delete(object sender, EventArgs e) {
-        }
-
-        public override void Save(object sender, EventArgs e) {
+            return tbtb;
         }
     }
-
-
 }
+
+//    class GamesPopupbox : FansyPopupBox {
+//        //for games
+//        public int gYear;
+//        public string gGenre = "";
+//        public int gMinTime;
+//        public int gMaxTime;
+//        public int gMinPlayers;
+//        public int gMaxPlayers;
+
+//        GamesPopupbox() {
+//            Text = "Game details";
+//        }
+
+//        public override void Delete(object sender, EventArgs e) {
+//        }
+
+//        public override void Save(object sender, EventArgs e) {
+//        }
+//    }
+
+
+//}
