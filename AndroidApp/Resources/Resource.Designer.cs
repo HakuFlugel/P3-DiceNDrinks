dd344c0de213f71e6896f115d52694a341be11ae
Lighdek <matiasholst@hotmail.com>
2016-10-28 14:58:35 +0200

    Added string for new button

--- AndroidApp/Resources/Resource.Designer.cs
+++ AndroidApp/Resources/Resource.Designer.cs
@@ -182,8 +182,17 @@ namespace AndroidApp
 			// aapt resource value: 0x7f050000
 			public const int button1 = 2131034112;
 			
+			// aapt resource value: 0x7f050005
+			public const int drinkButton = 2131034117;
+			
 			// aapt resource value: 0x7f050001
 			public const int exit = 2131034113;
+			
+			// aapt resource value: 0x7f050004
+			public const int foodButton = 2131034116;
+			
+			// aapt resource value: 0x7f050006
+			public const int miscButton = 2131034118;
 			
 			// aapt resource value: 0x7f050003
 			public const int no = 2131034115;
