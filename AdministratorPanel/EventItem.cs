7907f937267f05afa81d241f6450304b79b36acf
hidefades <hiddenfades@gmail.com>
2016-10-28 10:52:53 +0200

    Work eventitem-Tab

---
+++ AdministratorPanel/EventItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdministratorPanel {
+    class EventItem : TableLayoutPanel {
+        public EventItem() {
+            RowCount = 1;
+            ColumnCount = 2;
+
+        }
+    }
+}
