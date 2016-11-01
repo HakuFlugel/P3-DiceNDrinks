a3213b55b01ecc0d77e4b868beb1b86b00c55eee
hidefades <hiddenfades@gmail.com>
2016-11-01 13:56:40 +0100

    Event changes

--- AdministratorPanel/EventItem.cs
+++ AdministratorPanel/EventItem.cs
@@ -14,14 +14,16 @@ namespace AdministratorPanel {
             RowCount = 1;
             ColumnCount = 2;
             BackColor = Color.LightGray;
-            Dock = DockStyle.Top;
+            Dock = DockStyle.Fill;
             AutoSize = true;
             AutoSizeMode = AutoSizeMode.GrowOnly;
             Margin = new Padding(4, 4, 20, 4);
             this.MakeSuperClickable((s, ev) => { this.BackColor = Color.Red; });
 
 
-            Controls.Add(new Label() { Text = "Dinmor"});
+            Controls.Add(new Label() { Text = "Sex with hem? ADOPTERET OG EN ULYKKE", AutoSize = true});
+            Controls.Add(new Label() { Font = new Font("Arial", 30), Text = "Fuck hem", AutoSize = true});
+            
         }
     }
 }
