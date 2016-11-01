b0d1dc4986ff3855d624c03248274028752a7d60
Xiazee <Xiazee@users.noreply.github.com>
2016-11-01 10:36:24 +0100

    Added hover effect to superclickable Removed unused usings

--- AdministratorPanel/ControlExtensions.cs
+++ AdministratorPanel/ControlExtensions.cs
@@ -1,34 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace AdministratorPanel {
-    public static class ControlExtensions {
+namespace AdministratorPanel
+{
+    public static class ControlExtensions
+    {
 
-        public static void MakeSuperClickable(this Control control, EventHandler clickEvent) { // TODO: hover events too?
+        public static void MakeSuperClickable(this Control control, EventHandler clickEvent)
+        {
+            // TODO: hover events too?
             ControlEventArgs cev = new ControlEventArgs(control);
 
-            clickEvent += (sender, ev) => { ((Control)sender).Focus(); };
+            clickEvent += (sender, ev) => { ((Control) sender).Focus(); };
 
             OnControlAdded(cev, clickEvent);
         }
 
-        private static void OnControlAdded(ControlEventArgs e, EventHandler clickEvent) {
-            ControlEventHandler controladded = (sender, ev) => {
+        private static Color smartRGB(params int[] par)
+        {
+            for (int i = 0; i < par.Length; i++)
+            {
+                par[i] = par[i] > 255 ? 255 : par[i] < 0 ? 0 : par[i];
+            }
 
-                foreach (Control control in e.Control.Controls) {
+
+            return Color.FromArgb(par[0], par[1], par[2]);
+        }
+
+        private static void OnControlAdded(ControlEventArgs e, EventHandler clickEvent)
+        {
+            ControlEventHandler controladded = (sender, ev) =>
+            {
+
+                foreach (Control control in e.Control.Controls)
+                {
                     ControlEventArgs cev = new ControlEventArgs(control);
                     OnControlAdded(cev, clickEvent);
                 }
+            };
+            EventHandler mouseEnter = (sender, ev) =>
+            {
+                Color b = e.Control.BackColor;
+                //e.Control.ForeColor = e.Control.BackColor;
+                e.Control.BackColor = smartRGB(b.R - 20, b.G - 20, b.B - 20);
+            };
+            EventHandler mouseLeave = (sender, ev) =>
+            {
+                Color b = e.Control.BackColor;
+                //e.Control.ForeColor = e.Control.BackColor;
+                e.Control.BackColor = smartRGB(b.R + 20, b.G + 20, b.B + 20);
             };
 
             e.Control.Click -= clickEvent;
             e.Control.Click += clickEvent;
             e.Control.ControlAdded -= controladded;
             e.Control.ControlAdded += controladded;
+
+            e.Control.MouseEnter -= mouseEnter;
+            e.Control.MouseEnter += mouseEnter;
+
+            e.Control.MouseLeave -= mouseLeave;
+            e.Control.MouseLeave += mouseLeave;
 
             controladded(e.Control, e);
         }
