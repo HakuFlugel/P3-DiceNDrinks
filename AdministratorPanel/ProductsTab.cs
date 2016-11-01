d2e9629b81910b6f7ba66a5c9d26c99b6fe56b06
GeneralDenmark <foo-fighters@live.dk>
2016-11-01 15:16:12 +0100

    I like butt

--- AdministratorPanel/ProductsTab.cs
+++ AdministratorPanel/ProductsTab.cs
@@ -1,7 +1,12 @@
 ﻿using System.Windows.Forms;
+using System.Collections.Generic;
+using Shared;
 
 namespace AdministratorPanel {
     public class ProductsTab : TabPage {
+
+        public List<Product> products;
+
         public ProductsTab() {
             Text = "Products";
             TabControl tb = new TabControl();
