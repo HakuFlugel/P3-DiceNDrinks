005843114aa52d1f1bea9a5052029c9ece5dc9a2
hidefades <hiddenfades@gmail.com>
2016-11-02 14:17:10 +0100

    Commiting my life force

--- AdministratorPanel/EventsTab.cs
+++ AdministratorPanel/EventsTab.cs
@@ -1,14 +1,41 @@
 ﻿using System.Drawing;
 using System.Windows.Forms;
 using System.Collections.Generic;
+using System.Linq;
 using Shared;
 
 namespace AdministratorPanel {
     public class EventsTab : TabPage {
         Form form;
-        public List<Event> Events = new List<Event>();
+        private List<Event> Events;
 
-        public EventsTab(Form form) {
+        public EventsTab(Form form, List<Event> Events) {
+
+            Events.Add(new Event() {
+                name = "Pandekage dag",
+                description = "This is a test",
+                startDate = new System.DateTime(2016, 11, 3, 22, 00, 00),
+                endDate = new System.DateTime(2016, 11, 3, 23, 00, 00)
+            });
+            Events.Add(new Event() {
+                name = "Pandekage dag",
+                description = "This is a test",
+                startDate = new System.DateTime(2016, 11, 3, 19, 00, 00),
+                endDate = new System.DateTime(2016, 11, 3, 23, 30, 00)
+            });
+            Events.Add(new Event() {
+                name = "Æbleskiver dag",
+                description = "This is a test",
+                startDate = new System.DateTime(2015, 11, 3, 22, 00, 00),
+                endDate = new System.DateTime(2015, 11, 3, 23, 00, 00)
+            });
+            Events.Add(new Event() {
+                name = "Nedren dag",
+                description = "This is a test",
+                startDate = new System.DateTime(2016, 10, 3, 22, 00, 00),
+                endDate = new System.DateTime(2016, 10, 6, 23, 00, 00)
+            });
+            this.Events = Events;
             this.form = form;
             Text = "Events";
 
@@ -31,10 +58,13 @@ namespace AdministratorPanel {
                 EventItem ei = new EventItem(new Event() { name = "Pandekage dag", description = "This is a test", startDate = new System.DateTime(2016, 11, 3, 22, 00, 00), endDate = new System.DateTime(2016, 11, 3, 23, 00, 00) });
                 lowertlp.Controls.Add(ei);
             }
-            foreach (var item in Events) {
 
-                //lowertlp.Controls.Add(ei);
+            Events.OrderBy<Da>
+            
+            foreach (var item in Events) {
+                lowertlp.Controls.Add(new EventItem(item));
             }
+
 
             Button addEvent = new Button();
             addEvent.Height = 20;
