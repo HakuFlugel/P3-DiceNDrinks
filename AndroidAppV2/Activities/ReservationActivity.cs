768335e1afcababb96932adcde6c65f7b42d3e32
Lighdek <matiasholst@hotmail.com>
2016-11-03 13:51:14 +0100

    I don't even care anymore
    (Reverted to AndroidApp and added a FormsLibrary)

---
+++ AndroidAppV2/Activities/ReservationActivity.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Android;
+
+namespace AndroidAppV2.Activities
+{
+    [Activity(Theme = "@style/Theme.NoTitle", Label = "Reservation")]
+    public class ReservationActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            
+            base.OnCreate(savedInstanceState);
+
+
+            SetContentView(Resource.Layout.ReservationLayout);
+            // Create your application here
+            DatePicker calender = FindViewById<DatePicker>(Resource.Id.datePicker1);
+            TimePicker clock = FindViewById<TimePicker>(Resource.Id.timePicker1);
+            Button button = FindViewById<Button>(Resource.Id.button1);
+            //calender.MinDateTime = System.DateTime.Today;
+            button.Click += delegate
+            {
+                button.Text =
+                    $"{calender.DayOfMonth}." +
+                    $"{calender.Month}." +
+                    $"{calender.Year}" +
+                    $" {clock.Hour}:" +
+                    $"{clock.Minute}";
+            };
+        }
+    }
+}
