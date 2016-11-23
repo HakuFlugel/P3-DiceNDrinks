021eba0ed8d580b30b16c749f6dce5b69eef4fa3
Lighdek <matiasholst@hotmail.com>
2016-11-23 09:51:30 +0100

    Async

--- AndroidAppV2/AndroidShared.cs
+++ AndroidAppV2/AndroidShared.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-
+using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
 using Android.Content.Res;
@@ -12,13 +12,15 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Java.IO;
 using Newtonsoft.Json;
 using Shared;
+using File = System.IO.File;
 using Path = System.IO.Path;
 
 namespace AndroidAppV2
 {
-    public static class AndroidShared
+    public class AndroidShared
     {
         public static void LoadData<T>(Context context, string file, out T type)
         {
@@ -66,5 +68,56 @@ namespace AndroidAppV2
             }
             return bitmap ?? (BitmapFactory.DecodeResource(context.Resources, Resource.Drawable.Icon));
         }
+
+        public async Task<BitmapFactory.Options> GetBitmapOptionsOfImageAsync(Context context, string path)
+        {
+            BitmapFactory.Options options = new BitmapFactory.Options
+            {
+                InJustDecodeBounds = true
+            };
+            // The result will be null because InJustDecodeBounds == true.
+
+            Stream file = context.Assets.Open(path);
+            
+            await BitmapFactory.DecodeStreamAsync(file, new Rect(), options);
+
+            return options;
+        }
+
+        public static int CalculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight)
+        {
+            // Raw height and width of image
+            float height = options.OutHeight;
+            float width = options.OutWidth;
+            double inSampleSize = 1D;
+
+            if (height > reqHeight || width > reqWidth)
+            {
+                int halfHeight = (int)(height / 2);
+                int halfWidth = (int)(width / 2);
+
+                // Calculate a inSampleSize that is a power of 2 - the decoder will use a value that is a power of two anyway.
+                while ((halfHeight / inSampleSize) > reqHeight && (halfWidth / inSampleSize) > reqWidth)
+                {
+                    inSampleSize *= 2;
+                }
+            }
+
+            return (int)inSampleSize;
+        }
+
+        public async Task<Bitmap> LoadScaledDownBitmapForDisplayAsync(Context context, string path, BitmapFactory.Options options, int reqWidth, int reqHeight)
+        {
+            // Calculate inSampleSize
+            options.InSampleSize = CalculateInSampleSize(options, reqWidth, reqHeight);
+
+            // Decode bitmap with inSampleSize set
+            options.InJustDecodeBounds = false;
+
+            Stream file = context.Assets.Open(path);
+
+            return await BitmapFactory.DecodeStreamAsync(file, new Rect(), options);
+        }
+
     }
 }
