8305cdeb2b21c6835ac0804fc5904f173c842564
matia <matia@DESKTOP-SQAM7N8>
2016-11-28 12:45:10 +0100

    alle kategorier tilføjet

--- AndroidAppV2/ListAdapters/ProductAdapter.cs
+++ AndroidAppV2/ListAdapters/ProductAdapter.cs
@@ -12,6 +12,7 @@ namespace AndroidAppV2.ListAdapters
 {
     internal class ProductAdapter : BaseAdapter<Product>
     {
+        private bool _default = true;
         private List<Product> _items;
         private List<Product> _categoryItems;
         private readonly List<Product> _baseItems;
@@ -48,29 +49,48 @@ namespace AndroidAppV2.ListAdapters
 
         public void SetListType(string type)
         {
-            _categoryItems = _baseItems.Where(prd => prd.category == type).ToList();
+            if (type != "Alt")
+            {
+                _categoryItems = _baseItems.Where(prd => prd.category == type).ToList();
+                _default = false;
+            }
+            else
+            {
+                _categoryItems = _baseItems;
+                _default = true;
+            }
         }
 
         public List<string> GetSections()
         {
             List<string> sections = new List<string>();
 
+            if (_default)
+            {
+                sections.Add("Alt");
+                return sections;
+            }
 
             foreach (var item in _categoryItems)
             {
                 if (!sections.Any(x => x.Contains(item.section)))
                 sections.Add(item.section);
             }
+
+
             if (sections.Count != 0)
                 return sections;
 
-            sections.Add("Nothing");
+            sections.Add("Ingenting");
                 return sections;
         }
         
         public void SetList(string section)
         {
-            _items = _baseItems.Where(prd => prd.section == section).ToList();
+            if (!_default)
+                _items = _baseItems.Where(prd => prd.section == section).ToList();
+            else
+                _items = _baseItems;
             NotifyDataSetChanged();
             GC.Collect();
         }
