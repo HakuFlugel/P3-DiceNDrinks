d7efdb47c9e7b254ba54b805fcd1f42811b62bd5
GeneralDenmark <foo-fighters@live.dk>
2016-12-08 15:33:30 +0100

    Fuk

--- AdministratorPanel/GamesTab/GamePopupBox.cs
+++ AdministratorPanel/GamesTab/GamePopupBox.cs
@@ -154,14 +154,7 @@ namespace AdministratorPanel {
 
         public GamePopupBox(GamesTab gametab, Game game, Genres genres) {
             Text = "Game";
-            Name = (game != null) ? game.name : "New Game";
-            if (Application.OpenForms[Name] as GamePopupBox != null) {
-                BringToFront();
-                return;
-            }
-                
             
-
             Size = new Size(500,640);
             this.genres = genres;
             this.gametab = gametab;
