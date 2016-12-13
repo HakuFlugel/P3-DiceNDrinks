9e504847cfcd7754894382f81193a038ea030444
HideFades <hiddenfades@gmail.com>
2016-12-13 13:44:36 +0100

    ContactInformation added to shared

---
+++ Shared/ContactInformation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Shared {
+    [Serializable]
+    class ContactInformation {
+        public string description;
+        public string email;
+        public string number;
+        public string[] location;
+        public string geoURL;
+        public string[] openingTimes;
+    }
+}
